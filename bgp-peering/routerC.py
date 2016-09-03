f223938ea84dbeb9f700bf40eae13a50b384ca6b
ajinkyakadam <ajinkya.kadam@gmail.com>
2016-09-03 19:18:59 -0400

    adding router and BGP configuration files

---
+++ bgp-peering/routerC.py
@@ -0,0 +1,83 @@
+### Author : Ajinkya Kadam
+### Date : 2nd September
+'''
+Step 1:
+     Router Configuration
+        login to router daemon process
+        hostname
+        login and privilged mode password
+        interface adresss
+'''
+try:
+    import pexpect
+except:
+    ImportError, "Pexpect module is missing, please install the module"
+
+try:
+    import re
+    import os
+except ImportError:
+    raise ImportError, "Exception occured when importing re and os module"
+
+try:
+    child = pexpect.spawn('telnet localhost 2601')
+except pexpect.ExceptionPexpect:
+    raise exceptions.NetworkException("Unable to spawn telnet process")
+else:
+    log_fileName = open('/tmp/routerClog', 'w')
+    child.logfile = log_fileName
+
+    index1 = child.expect([r'.refused',r'Vty password is not set.','Password: '])
+    print "index1 is ", index1
+    if index1 == 0:
+        print "Either quagga process is stoped , please run sudo /etc/init.d/quagga start"
+    elif index1 == 1:
+        print "Vty password not set. Please set vty password in file /etc/quagga/zebra.conf"
+    elif index1 == 2:
+        child.sendline('routerC')
+        child.expect('.+>')
+
+        ### Check if privilege mode password is set or not
+        ###  if not then  only set the password or else
+        ###   provide the password
+        child.sendline('enable')
+        index2 = child.expect(['Password: ','.+#'])
+
+        if index2 == 0:
+            child.sendline('routerC')
+            child.expect('.+#')
+            child.sendline('configure terminal')
+        elif index2 == 1:
+            child.sendline('configure terminal')
+            child.expect('.+#')
+            child.sendline('enable password routerC')
+            child.expect('.+#')
+
+        child.sendline('service password-encryption')
+        child.expect('.+#')
+        child.sendline('hostname routerC')
+        child.expect('.+#')
+
+        ## get interface name
+        iface1 = re.search(r'e\w+\d',os.popen('ip addr | grep "10.1.3.2"').read()).group()
+        iface2 = re.search(r'e\w+\d',os.popen('ip addr | grep "10.1.4.1"').read()).group()
+        child.sendline('interface '+iface1)
+        child.expect('.+#')
+        child.sendline('description Interface connected to router B which is a eBGP peer')
+        child.expect('.+#')
+        child.sendline('ip address 10.1.3.2/30')
+        child.expect('.+#')
+        child.sendline('interface '+iface2)
+        child.expect('.+#')
+        child.sendline('description Interface connected to router D which is a iBGP peer')
+        child.expect('.+#')
+        child.sendline('ip address 10.1.4.1/30')
+        child.expect('[\w()-]#')
+        child.sendline('exit')
+        child.expect('.+#')
+        child.sendline('exit')
+        child.expect('.+#')
+        child.sendline('copy running-config startup-config')
+finally:
+    child.close()
+    log_fileName.close()
---
+++ bgp-peering/routerCbgpprocess.py
@@ -0,0 +1,77 @@
+### Author : Ajinkya Kadam
+### Date : 2nd September
+'''
+Step 2:
+     Router BGP Configuration
+        login to bgp daemon process
+        login and privilged mode password
+        hostname
+        annonuce networks
+        configure neighbor
+'''
+try:
+    import pexpect
+except:
+    ImportError, "Pexpect module is missing, please install the module"
+
+try:
+    import re
+    import os
+except ImportError:
+    raise ImportError, "Exception occured when importing re and os module"
+
+try:
+    child = pexpect.spawn('telnet localhost 2604')
+except pexpect.ExceptionPexpect:
+    raise exceptions.NetworkException("Unable to spawn telnet process")
+else:
+    log_fileName = open('/tmp/routerCbgplog', 'w')
+    child.logfile = log_fileName
+
+    index1 = child.expect([r'.refused',r'Vty password is not set.','Password: '])
+    print "index1 is ", index1
+    if index1 == 0:
+        print "Either quagga process is stoped , please run sudo /etc/init.d/quagga start"
+    elif index1 == 1:
+        print "Vty password not set. Please set vty password in file /etc/quagga/zebra.conf"
+    elif index1 == 2:
+        child.sendline('routerC')
+        child.expect('.+>')
+
+        ### Check if privilege mode password is set or not
+        ###  if not then  only set the password or else
+        ###   provide the password
+        child.sendline('enable')
+        index2 = child.expect(['Password: ','.+#'])
+
+        if index2 == 0:
+            child.sendline('routerC')
+            child.expect('.+#')
+            child.sendline('configure terminal')
+        elif index2 == 1:
+            child.sendline('configure terminal')
+            child.expect('.+#')
+            child.sendline('enable password routerC')
+            child.expect('.+#')
+
+        child.sendline('service password-encryption')
+        child.expect('.+#')
+        child.sendline('hostname routerC')
+        child.expect('.+#')
+
+        child.sendline('router bgp 65200')
+        child.expect('.+#')
+        child.sendline('network 10.1.3.0 mask 255.255.255.252')
+        child.expect('.+#')
+        child.sendline('neighbor 10.1.3.1 remote-as 65100')
+        child.expect('.+#')
+        child.sendline('neighbor 10.1.4.2 remote-as 65200')
+        child.expect('.+#')
+        child.sendline('exit')
+        child.expect('.+#')
+        child.sendline('exit')
+        child.expect('.+#')
+        child.sendline('copy running-config startup-config')
+finally:
+    child.close()
+    log_fileName.close()
