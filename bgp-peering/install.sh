ed45461cc35fdd31148a6e5b9a545c257b330360
ajinkyakadam <ajinkya.kadam@gmail.com>
2016-09-03 01:38:17 -0400

    replacing nano with touch

--- bgp-peering/install.sh
+++ bgp-peering/install.sh
@@ -8,9 +8,9 @@ sudo apt-get install -y quagga
 echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf 
 echo "net.ipv4.conf.default.forwarding=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p 
-sudo nano /etc/quagga/bgpd.conf 
-sudo nano /etc/quagga/vtysh.conf 
-sudo nano /etc/quagga/zebra.conf 
+sudo touch /etc/quagga/bgpd.conf 
+sudo touch /etc/quagga/vtysh.conf 
+sudo touch /etc/quagga/zebra.conf 
 
 sudo chown quagga:quaggavty /etc/quagga/vtysh.conf && sudo chmod 660 /etc/quagga/vtysh.conf 
 sudo chown quagga:quagga /etc/quagga/zebra.conf && sudo chmod 640 /etc/quagga/zebra.conf 
